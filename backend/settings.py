7d7d358723c7dd61ae3e2c45ba12f96f5a2ae15c
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2025-04-15 11:01:25 -0500

    base de datos y proyecto arrancado

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sabores'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'YRvaHSDuDcFmzndEcSghfkGMcpAZAZlb',
+        'HOST': 'metro.proxy.rlwy.net',  # Usa el valor real de RAILWAY_PRIVATE_DOMAIN
+        'PORT': '53920',
     }
 }
 
