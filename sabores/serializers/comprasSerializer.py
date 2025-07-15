adf91c1d4e8b109073385d9a54c403773d2f93a2
JhoanElMejor <jhoancordoba72@gmail.com>
2025-07-15 01:41:13 -0500

    Actualización anidadas de compras y detallesCompras

--- sabores/serializers/comprasSerializer.py
+++ sabores/serializers/comprasSerializer.py
@@ -41,3 +41,27 @@ class ComprasSerializer(serializers.ModelSerializer):
             return compra
         except Exception as e:
             return {"Error": e};
+
+    def update(self, instance, validated_data):
+        detalles_data = validated_data.pop('detallesCompra')
+        
+        # Actualizar la compra principal
+        instance = super().update(instance, validated_data)
+        
+        # Actualizar o crear cada detalle
+        detalles = instance.detallesCompra.all()
+        detalles = {detalle.id: detalle for detalle in detalles}
+        
+        for detalle_data in detalles_data:
+            detalle_id = detalle_data.get('id', None)
+            if detalle_id in detalles:
+                detalle_serializer = DetallesComprasSerializer(
+                    detalles[detalle_id], 
+                    data=detalle_data
+                )
+                detalle_serializer.is_valid(raise_exception=True)
+                detalle_serializer.save()
+            else:
+                instance.detallesCompra.create(**detalle_data)
+        
+        return instance
