b54aefa3d56ff1ebc7789700aa1cc389f705834e
JhoanElMejor <jhoancordoba72@gmail.com>
2025-07-23 18:26:57 -0500

    Eliminación masiva en categorías
    Puede eliminar varios registro a la vez

--- sabores/views/categoriasView.py
+++ sabores/views/categoriasView.py
@@ -18,6 +18,13 @@ class CategoriasView(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['nombre'] # ''
 
+
+    @action(detail=False, methods=['post'])
+    def bulk_delete(self, request):
+        ids = request.data.get('ids', [])
+        self.queryset.filter(id__in=ids).delete()
+        return Response(status=204)
+    
     # @action(detail=False, methods=['POST'])
     # def eliminar_productos(self, request):
 
